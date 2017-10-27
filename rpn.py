0ae8decc243beed2335de446271e189a7393d4e6
Jcc <jccccccc@umich.edu>
2017-10-27 15:10:27 -0400

    Almost done version

--- rpn.py
+++ rpn.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
-def calculate(arg):
-	pass
+
+
+
+def add(a,b):
+	return a+b
+
+def subtract(a,b):
+	return a-b
+
+ops={
+	'+': operator.add,
+	'-': operator.subtract
+}
+
+def calculate(myarg):
+	stack=list()
+	for token in myarg.split():
+		try:
+			stack.append(int(token))
+		except ValueError:
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			function = ops[token]
+			result=function(arg1,arg2)
+			stack.append(result)
+	print(stack)
+	return stack.pop()
 
 def main():
 	while True:
@@ -8,3 +33,4 @@ def main():
 
 if __name__=='__main__':
 	main()
+
--- test_rpn.py
+++ test_rpn.py
@@ -1,7 +1,13 @@
 import unittest
+
 import rpn
 
 class TestBasics(unittest.TestCase):
 	def test_add(self):
-		result = rpn.calculate("1 1 +")
+		result = rpn.calculate('1 1 +')
 		self.assertEqual(2,result)
+
+	def test_subtract(self):
+		result =rpn.calculate('5 3 -')
+		self.assertEqual(2,result)
+
