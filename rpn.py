cec6d05ad3e1ce4f6c69f5f2a5e6c131b3f48304
Jcc <jccccccc@umich.edu>
2017-11-02 16:34:03 -0400

    Final version of rpn

--- rpn.py
+++ rpn.py
@@ -8,6 +8,7 @@ operators = {
     '-': operator.sub,
     '*': operator.mul,
     '/': operator.truediv,
+    '^': operator.pow,
 }
 
 def calculate(myarg):
--- test_rpn.py
+++ test_rpn.py
@@ -15,3 +15,6 @@ class TestBasics(unittest.TestCase):
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
+    def test_expo(self):
+        result = rpn.calculate("2 3 ^")
+        self.assertEqual(8,result)
